d0b97a3116060e37e6e147665bbebc8d63c3f194
iota <ishu2000pandey@gmail.com>
2024-05-29 15:20:13 +0530

    added apis for login, logout and verifyemail

---
+++ src/app/api/users/login/route.ts
@@ -0,0 +1,50 @@
+import {connect} from '@/dbConfig/dbConfig'
+import User from '@/models/userModel'
+import { NextRequest, NextResponse } from 'next/server'
+import bcryptjs from 'bcryptjs'
+import { sendEmail } from '@/helpers/mailer'
+import jwt from 'jsonwebtoken'
+
+connect();
+
+export async function POST(req: NextRequest) {
+    try {
+        const reqBody = await req.json()
+        const { email, password } = reqBody
+        console.log(reqBody)
+
+        const user = await User.findOne({email})
+
+        if(!user) {
+            return NextResponse.json({error: "User does not exist"}, {status: 400})
+        }
+        console.log("user exists");
+
+        const validPassword = await bcryptjs.compare(password, user.password)
+
+        if(!validPassword) {
+            return NextResponse.json({error: "Check your credentials"}, {status: 400})
+        }
+        
+        const tokenData = {
+            id: user._id,
+            username: user.username,
+            email: user.email
+        }
+
+    const token = jwt.sign(tokenData, process.env.TOKEN_SECRE!, {expiresIn: '1d'})
+
+    const response = NextResponse.json({
+        message: "Logged in successfully",
+        success: true,
+    })
+
+    response.cookies.set("token", token, {
+        httpOnly: true,
+    })
+    return response
+
+    } catch(error: any) {
+        return NextResponse.json({error: error.message}, {status: 500})
+    }
+}
